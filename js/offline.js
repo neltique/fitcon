9642a216096c4f24bfc4aadfcf507d6b37959c2f
neltique <neltique123@gmail.com>
2023-09-01 17:56:22 +0900

    class end

---
+++ js/offline.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('.center_auto').slick({
+        centerMode: true,
+        centerPadding: '60px',
+
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+
+
+    });
+
+});
